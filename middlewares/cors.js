b49a6f382b21501ab673e98dffff82abe7a4300f
inbalf33 <inbalf33@gmail.com>
2026-09-22 08:55:26 +0300

    fix: add vercel frontend URL to CORS origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,7 +5,8 @@ const corsmiddleware = cors({
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "https://jobs-app-puce.vercel.app"
     ],
     allowedHeaders: ["Content-Type", "x-auth-token", "Authorization"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
